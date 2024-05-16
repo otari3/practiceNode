b335b729b1cb633fddbffcd37b2fadd5033da631
otari nozadze <otikonozadze@gmail.com>
2024-05-17 00:53:19 +0400

    t

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
+app.use;
 
 app.use(shopRoutes);
 app.use((req, res, next) => {
