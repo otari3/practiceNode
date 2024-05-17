bc1271f27b3ba4dbd2dece410f00c97cb9062481
otari nozadze <otikonozadze@gmail.com>
2024-05-17 12:57:52 +0400

    added ffake databae

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const app = express();
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes.router);
 
 app.use(shopRoutes);
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const path = require("path");
 
 const rootDir = require("../helpers/path");
+
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body.title);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
-module.exports = router;
+module.exports = { router, products };
