ccca8736db2a0675bc1cfa36377efd82782aa51f
otari nozadze <otikonozadze@gmail.com>
2024-05-18 17:35:10 +0400

    learning pug

--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const rootDir = require("../helpers/path");
+const products = require("./admin");
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // console.log(products.products);
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop");
 });
 module.exports = router;
