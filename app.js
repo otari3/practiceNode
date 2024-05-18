1e954a94ab66c9a0cc6f14f672942fac93f711e4
otari nozadze <otikonozadze@gmail.com>
2024-05-18 20:00:46 +0400

    learning handlebars

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-
+const expressHbs = require("express-handlebars");
 const app = express();
-
-app.set("view engine", "pug");
-app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
+app.engine("hbs", expressHbs());
+app.set("view engine", "hbs");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -18,7 +19,8 @@ app.use("/admin", adminRoutes.router);
 
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -8,6 +8,6 @@ router.get("/", (req, res, next) => {
   // console.log(products.products);
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
   const products = adminData.products;
-  res.render("shop", { prods: products, docTitle: "Shop" });
+  // res.render("shop", { prods: products, docTitle: "Shop" });
 });
 module.exports = router;
