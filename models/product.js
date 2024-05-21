4c97e0279ea8c4bd381df8a5d4883a17b2243ac2
otari nozadze <otikonozadze@gmail.com>
2024-05-21 16:41:35 +0400

    cb

--- models/product.js
+++ models/product.js
@@ -1,12 +1,32 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
   save() {
-    products.push(this);
+    fs.readFile(p, (err, data) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(data);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    fs.readFile(p, (err, data) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(data));
+    });
   }
 };
